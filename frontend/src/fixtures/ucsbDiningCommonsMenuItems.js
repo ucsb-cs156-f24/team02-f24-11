f9a6e12e27590fd52eaa07573d897096de2151f1
smmzhu <smmzhu@gmail.com>
2024-10-29 21:33:37 -0700

    added fixture for ucsbdiningcommonmenuitems

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItems.js
@@ -0,0 +1,30 @@
+const ucsbDiningCommonsMenuItems = {
+  oneDate: {
+    id: 1,
+    diningCommonsCode: "DLG",
+    name: "Hot Dog",
+    station: "West Side",
+  },
+  threeDates: [
+    {
+      id: 1,
+      diningCommonsCode: "Carrillo",
+      name: "Pepperoni Pizza",
+      station: "Mountain Side",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "Portola",
+      name: "Cantaloupe",
+      station: "Salad Bar",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "Ortega",
+      name: "Macaroni",
+      station: "Takeout",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItems };
