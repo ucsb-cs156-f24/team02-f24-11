890ca4eb6c5ea1d93bd56dd8982f6a321dfc6eea
Frank Wang <frankningwang@gmail.com>
2024-11-01 19:17:30 -0700

    Frank add fixtures closes #54

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,38 @@
+const ucsbDatesFixtures = {
+  oneArticle: {
+    id: 1,
+    title: "Article One",
+    url: "one.com",
+    explanation: "This is article one",
+    email: "one@gmail.com",
+    dateAdded: "2020-11-11T11:11:11",
+  },
+  threeArticles: [
+    {
+      id: 1,
+      title: "Article One",
+      url: "one.com",
+      explanation: "This is article one",
+      email: "one@gmail.com",
+      dateAdded: "2020-11-11T11:11:11",
+    },
+    {
+      id: 2,
+      title: "Article Two",
+      url: "two.com",
+      explanation: "This is article two",
+      email: "two@gmail.com",
+      dateAdded: "2020-12-12T12:12:12",
+    },
+    {
+      id: 1,
+      title: "Article Three",
+      url: "three.com",
+      explanation: "This is article three",
+      email: "three@gmail.com",
+      dateAdded: "2020-11-13T13:13:13",
+    },
+  ],
+};
+
+export { ucsbDatesFixtures };
