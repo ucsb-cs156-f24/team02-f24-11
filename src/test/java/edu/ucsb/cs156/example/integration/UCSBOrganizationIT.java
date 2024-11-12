bb1cfd4cd3f66993fb9f734aef05110bfc8c6b13
Aden <adenjo@ucsb.edu>
2024-11-12 13:42:55 -0800

    Errors

---
+++ src/test/java/edu/ucsb/cs156/example/integration/UCSBOrganizationIT.java
@@ -0,0 +1,61 @@
+// package edu.ucsb.cs156.example.integration;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.context.annotation.Import;
+// import org.springframework.security.test.context.support.WithMockUser;
+// import org.springframework.test.annotation.DirtiesContext;
+// import org.springframework.test.annotation.DirtiesContext.ClassMode;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.context.junit.jupiter.SpringExtension;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.MvcResult;
+
+// import com.fasterxml.jackson.databind.ObjectMapper;
+
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+// import edu.ucsb.cs156.example.entities.UCSBOrganizations;
+// import edu.ucsb.cs156.example.repositories.RestaurantRepository;
+// import edu.ucsb.cs156.example.repositories.UCSBOrganizationsRepository;
+// import edu.ucsb.cs156.example.repositories.UserRepository;
+// import edu.ucsb.cs156.example.services.CurrentUserService;
+// import edu.ucsb.cs156.example.services.GrantedAuthoritiesService;
+// import edu.ucsb.cs156.example.testconfig.TestConfig;
+
+
+// @ExtendWith(SpringExtension.class)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @AutoConfigureMockMvc
+// @ActiveProfiles("integration")
+// @Import(TestConfig.class)
+// @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+
+// public class UCSBOrganizationIT {
+//      @Autowired
+//         public CurrentUserService currentUserService;
+
+//         @Autowired
+//         public GrantedAuthoritiesService grantedAuthoritiesService;
+
+//         @Autowired
+//         RestaurantRepository restaurantRepository;
+
+//         @Autowired
+//         public MockMvc mockMvc;
+
+//         @Autowired
+//         public ObjectMapper mapper;
+
+//         @MockBean
+//         UserRepository userRepository;
+    
+// }
